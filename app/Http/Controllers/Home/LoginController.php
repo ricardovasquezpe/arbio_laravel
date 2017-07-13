eac720f54df55b2214e2c3d5455e381a2b552d59
Kevin Campo <kevin.campo@bitsngeeks.com>
2017-07-13 12:06:11 -0500

    Adding guzzle

--- app/Http/Controllers/Home/LoginController.php
+++ app/Http/Controllers/Home/LoginController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers\Home;
 
+use GuzzleHttp\Client;
 use App\Http\Controllers\Controller;
 
 class LoginController extends Controller {
 
     public function index()
     {
+    	/*$client = new Client();
+		$res = $client->get('https://api.github.com/user', ['auth' =>  ['ricardo.vasquez.pe@gmail.com', 'pimienta123']]);
+		echo $res->getStatusCode();
+		echo $res->getBody();*/
         return view('home.login');
+    }
+
+    public function login(){
+
     }
 }
